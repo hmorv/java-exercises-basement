443e784433821cac834a2dabf01d5cfae5f5719a
Hugo Moragues <hugo.moragues@gmail.com>
2018-01-12 13:17:06 +0100

    mmm
    yep, I'm kinda drunk (it's friday), but this afternoon it will be completed xD

--- ex9/EvenNumbers.java
+++ ex9/EvenNumbers.java
@@ -1,5 +1,58 @@
 package ex9;
 
-public class EvenNumbers {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.ArrayList;
 
+public class EvenNumbers {
+	protected InputStreamReader isr;
+	protected BufferedReader buff;
+	
+	public EvenNumbers() {
+		this.isr = new InputStreamReader(System.in);
+		this.buff = new BufferedReader(this.isr);
+	}
+	
+	public int checkRange(int min, int max) {
+		//ArrayList<Integer> result = new ArrayList<>();
+		int total = 0;
+		
+		for(int counter = min; min <= max; counter++) {
+			if(this.isEven(counter)) {
+				//result.add(counter);
+				total++;
+			}
+		}
+		
+		//System.out.println(String.format("Min value: %d, max value:", min, max));
+		
+		return total;
+	}
+	public int readFromKeyboard() {
+		int value;
+
+		try {
+			System.out.println("Write a number: ");
+			value = Integer.parseInt(this.buff.readLine());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			value = -1;
+		} catch(NumberFormatException e) {
+			System.out.println("The value entered is not a number!");
+			value = -1;
+		}
+			
+		return value;
+	}
+	
+	private boolean isEven(int num) {
+		return num % 2 == 0;
+	}
+	
+	private boolean isOdd(int num) {
+		return num % 2 != 0;
+	}
 }
--- ex9/EvenNumbersTest.java
+++ ex9/EvenNumbersTest.java
@@ -4,7 +4,18 @@ public class EvenNumbersTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		EvenNumbers en = new EvenNumbers();
+		
+		int min, max;
+		
+		min = en.readFromKeyboard();
+		max = en.readFromKeyboard();
+		
+		int result = en.checkRange(min, max);
+		
+		System.out.println(String.format("even nums between %d & %d: %d", min, max, result));
+		
 	}
 
 }
