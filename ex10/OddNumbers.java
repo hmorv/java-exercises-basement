9b2a36f86923cd92850c8a387ba328e9a940b84a
Hugo Moragues <hugo.moragues@gmail.com>
2018-02-09 15:04:36 +0100

    ex10

--- ex10/OddNumbers.java
+++ ex10/OddNumbers.java
@@ -1,5 +1,54 @@
 package ex10;
 
-public class OddNumbers {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class OddNumbers {
+	
+	protected InputStreamReader isr;
+	protected BufferedReader buff;
+	
+	public OddNumbers() {
+		this.isr = new InputStreamReader(System.in);
+		this.buff = new BufferedReader(this.isr);
+	}
+	
+	public int checkRange(int min, int max) {
+		//ArrayList<Integer> result = new ArrayList<>();
+		int total = 0;
+		
+		for(int counter = min; counter <= max; counter++) {
+			if(this.isOdd(counter)) {
+				//result.add(counter);
+				total++;
+			}
+		}
+		
+		return total;
+	}
+	
+	public int readFromKeyboard() {
+		
+		int value;
+
+		try {
+			System.out.println("Write a number: ");
+			value = Integer.parseInt(this.buff.readLine());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			value = -1;
+		} catch(NumberFormatException e) {
+			System.out.println("The value entered is not a number!");
+			value = -1;
+		}
+			
+		return value;
+	}
+	
+	private boolean isOdd(int num) {
+		return num % 2 != 0;
+	}
 }
--- ex10/OddNumbersTest.java
+++ ex10/OddNumbersTest.java
@@ -3,8 +3,21 @@ package ex10;
 public class OddNumbersTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		OddNumbers on = new OddNumbers();
+		
+		int min, max;
+		
+		min = on.readFromKeyboard();
+		max = on.readFromKeyboard();
+		if(max<=min) {
+			System.out.println(String.format("%d is not < %d", min, max));
+			return;
+		}
+		
+		int result = on.checkRange(min, max);
+		
+		System.out.println(String.format("odd nums between %d & %d: %d", min, max, result));
+	
 	}
-
 }
